401f8f9fe8dc7d4444961f19246e174a1834aaf1
Thomas Schreiner <t.schreiner@gmail.com>
2013-12-02 21:59:28 +0100

    First working version
    but horribly slow

--- Program.cs
+++ Program.cs
@@ -7,37 +7,47 @@ namespace Rubik
 {
     class Program
     {
-        static char[][] spielfeld = new char[24][];
-        
+        private static long AnzahlZuege = -1;
+        private static char[][] Spielfeld;
+        private static DateTime LastTime = DateTime.Now;
 
         static void Main(string[] args)
         {
             List<char[]> cards = CreateCards();
+            Spielfeld =  new char[cards.Count][];
+
             foreach (var card in cards)
             {
                 Console.WriteLine(card);
             }
-            spielfeld[0] = cards[0];
-            cards.RemoveAt(0);
-            LegeKarte(1, cards);
+
+            LegeKarte(0, cards);
+            Console.WriteLine("keine lösung gefunden.");
             Console.ReadLine();
         }
 
         private static void LegeKarte(int spielfeldPos, List<char[]> cards)
         {
+            AnzahlZuege += 1;
+            if(AnzahlZuege%1000000==0)
+            {
+                TimeSpan timeSpan = DateTime.Now - LastTime;
+                Console.WriteLine(string.Format("{0:mmss}\tZüge: {1}\tRestkarten: {2}", timeSpan, AnzahlZuege, cards.Count));
+                LastTime = DateTime.Now;
+            }
             if(cards.Count == 0)
             {
                 //fertig?
-                Console.WriteLine("Fertig");
+                Console.WriteLine("Fertig; Züge: " + AnzahlZuege);
                 Console.ReadLine();
             }
             for(int i = 0; i < cards.Count; i++)
             {
                 for (int rotations = 0; rotations < 4; rotations++)
                 {
-                    if (CompareRechts(spielfeld[spielfeldPos - 1], cards[i]))
+                    if (Compare(spielfeldPos, cards[i]))
                     {
-                        spielfeld[spielfeldPos] = cards[i];
+                        Spielfeld[spielfeldPos] = cards[i];
                         var newCards = new List<char[]>(cards);
                         newCards.RemoveAt(i);
                         LegeKarte(spielfeldPos+1, newCards);
@@ -58,9 +68,23 @@ namespace Rubik
             cards[pos] = rotatedCard;
         }
 
-        private static bool CompareRechts(char[] links, char[] rechts)
+        private static bool Compare(int pos, char[] card)
         {
-            return links[2] == rechts[7] && links[3] == rechts[6];
+            int posLinks = pos - 1;
+            bool vglLinks = true;
+            bool vglOben  = true;
+            if (posLinks % 5 != 0 && posLinks >= 0)
+            {
+                var links = Spielfeld[posLinks];
+                vglLinks = links[2] == card[7] && links[3] == card[6];
+            }
+            int posOben = pos - 5;
+            if(posOben >= 0)
+            {
+                var oben = Spielfeld[posOben];
+                vglOben = oben[5] == card[0] && oben[4] == card[1];
+            }
+            return vglLinks && vglOben;
         }
 
         private static List<char[]> CreateCards()
@@ -77,7 +101,9 @@ namespace Rubik
                         foreach (char c4 in new[] { 'a', 'b', 'c', 'd' })
                         {
                             if (c1 == c4 || c2 == c4 || c3 == c4) continue;
-                            result.Add(new []{c1,c2,c2,c3,c4,c1,c4,c3});
+                            result.Add(new []{c1,c2,c3,c2,c4,c1,c3,c4});
+                            if(result.Count == 1 ) // ein teil ist doppelt
+                                result.Add(new[] { c1, c2, c3, c2, c4, c1, c3, c4 });
                         }
                     }
                 }
